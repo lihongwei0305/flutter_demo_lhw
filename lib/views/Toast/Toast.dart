ea15e20018ed8fe1d9ab9405496fecd731f817ce
lihw <lihw@gov.in>
2023-02-28 09:28:35 +0800

    toast

--- lib/views/Toast/Toast.dart
+++ lib/views/Toast/Toast.dart
@@ -21,22 +21,58 @@ class _ToastPageState extends State<ToastPage> {
       ),
       body: Column(
         children: [
-          Center(
-            child: CupertinoButton.filled(
-                child: Text('toast'),
-                onPressed: () async {
-                  EasyLoading.showToast("toast");
-                }),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: CupertinoButton.filled(
+                  child: Text('toast'),
+                  onPressed: () async {
+                    EasyLoading.showToast("toast");
+                  }),
+            ),
           ),
-          Center(
-            child: CupertinoButton.filled(
-                child: Text('loading'),
-                onPressed: () async {
-                  EasyLoading.show();
-                  Timer(Duration(seconds: 3), () {
-                    EasyLoading.dismiss();
-                  });
-                }),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: CupertinoButton.filled(
+                  child: Text('success'),
+                  onPressed: () async {
+                    EasyLoading.showSuccess("success");
+                  }),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: CupertinoButton.filled(
+                  child: Text('error'),
+                  onPressed: () async {
+                    EasyLoading.showError("error");
+                  }),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: CupertinoButton.filled(
+                  child: Text('showProgress'),
+                  onPressed: () async {
+                    EasyLoading.showProgress(0.3, status: 'downloading...');
+                  }),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: CupertinoButton.filled(
+                  child: Text('loading'),
+                  onPressed: () async {
+                    EasyLoading.show();
+                    Timer(Duration(seconds: 3), () {
+                      EasyLoading.dismiss();
+                    });
+                  }),
+            ),
           )
         ],
       ),
